3f2eaee9d99fb01889f99b02709ec766f6b3efdb
Tomás Barros <133578690+tomas-barros1@users.noreply.github.com>
2024-03-27 15:41:47 -0300

    Update JogoDaForca.java

--- src/JogoDaForca.java
+++ src/JogoDaForca.java
@@ -10,25 +10,21 @@ public class JogoDaForca {
         int vidas = 6;
         Scanner s1 = new Scanner(System.in);
 
-        // Inicializa a palavra escondida com "_ "
         for (int i = 0; i < palavraSorteada.length(); i++) {
             palavraEscondida += "_ ";
         }
 
         do {
-            // Imprime a palavra escondida com espaços entre cada letra
             System.out.println(palavraEscondida.replaceAll("(.)", "$1 "));
             System.out.println("Numero de vidas: " + vidas);
             System.out.println("Digite uma letra da palavra: ");
             String letraInput = s1.nextLine();
 
             boolean letraCorreta = false;
-            char letra = letraInput.charAt(0); // Obter a primeira letra digitada
+            char letra = letraInput.charAt(0); 
 
-            // Verifica se a letra está presente na palavra
             for (int i = 0; i < palavraSorteada.length(); i++) {
                 if (palavraSorteada.charAt(i) == letra) {
-                    // Atualiza a palavra escondida para mostrar a letra correta
                     palavraEscondida = palavraEscondida.substring(0, 2 * i) + letra + palavraEscondida.substring(2 * i + 1);
                     letraCorreta = true;
                 }
